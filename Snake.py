04901c16ed5b5b4e4d750b9b2811850dd350ee87
MEtimebroo <87042061+MEtimebroo@users.noreply.github.com>
2026-04-17 13:15:27 -0600

    Refactor player initialization and rendering logic
    Renders player movement and makes the edges of the map into walls

--- Snake.py
+++ Snake.py
@@ -1,7 +1,21 @@
+import sys
+
 class Player:    
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+def render():
+    for i in range(height):
+        for j in range(width):
+            if i == play.y and j == play.x and j < width - 1:
+                print('#', end='')
+            elif i == play.y and j == play.x:
+                print('#')
+            elif j < width - 1:
+                print('.', end='')
+            else:
+                print('.')
 
 print('snake by chuh and ric')
 
@@ -11,11 +25,38 @@ width = int(input())
 print('How tall do you want the map to be?')
 height = int(input())
 
-play = Player(width / 2 + 1, height / 2 + 1)
+play = Player(int(width / 2), int(height / 2))
 
-for i in range(height):
-    for j in range(width):
-            if j < width - 1:
-                print('.', end='')
-            else:
-                print('.')
+render()
+
+while True:
+    print('Which direction would you like to move? (WASD)')
+    mov = input().lower()
+
+    if mov == 'w':
+        play.y -= 1
+    elif mov == 'a':
+        play.x -= 1
+    elif mov == 's':
+        play.y += 1
+    elif mov == 'd':
+        play.x += 1
+    elif mov == 'exit' or mov == 'e':
+        sys.exit()
+    else:
+        print('[ERROR]: Not a move!')
+    
+    if play.x >= width:
+        play.x = width - 1
+    
+    if play.x <= 0:
+        play.x = 0
+    
+    if play.y >= height:
+        play.y = height - 1
+    
+    if play.y <= 0:
+        play.y = 0
+
+    render()
+
