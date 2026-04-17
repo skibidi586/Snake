40a3bbce6d46c8be2fc20da235383c7f0e3dff3a
MEtimebroo <87042061+MEtimebroo@users.noreply.github.com>
2026-04-17 12:15:13 -0600

    Implement Player class for game initialization
    Added Player class to initialize player position and added map functionality.

--- Snake.py
+++ Snake.py
@@ -1,1 +1,21 @@
+class Player:    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 print('snake by chuh and ric')
+
+print('How wide do you want the map to be?')
+width = int(input())
+
+print('How tall do you want the map to be?')
+height = int(input())
+
+play = Player(width / 2 + 1, height / 2 + 1)
+
+for i in range(height):
+    for j in range(width):
+            if j < width - 1:
+                print('.', end='')
+            else:
+                print('.')
